f10732b3a7709667cdb5dd4cbaf090987d7343eb
Ygor Galindo <ygorvalerio@gmail.com>
2024-08-14 20:19:47 -0300

    first commit

---
+++ Musica.cs
@@ -0,0 +1,30 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public string DescricaoResumida => $"A música {Nome} pertenece à banda {Artista}";
+                
+        
+            
+      
+    
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: "+ Nome);
+        Console.WriteLine($"Artista: " + Artista);
+        Console.WriteLine($"Duração: " + Duracao);
+
+        if (Disponivel)
+        {
+            Console.WriteLine($"Disponivel no Plano");
+        }
+        else {
+            Console.WriteLine("Adquira o Plano Plus+");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Screen Sound
+
+Musica musica1 = new Musica();
+
+musica1.Nome = "Losing My Religion";
+musica1.Artista = "R.E.M";
+musica1.Duracao = 260;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+
+Musica musica2 = new Musica();
+musica2.Nome = "Wind of Change";
+musica2.Artista = "Scorpions";
+musica2.Duracao = 240;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.DescricaoResumida);
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
