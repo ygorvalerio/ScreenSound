3937d3d73e0879d5ddf08290e5d3d54468f275f9
Ygor Galindo <ygorvalerio@gmail.com>
2024-08-16 18:16:54 -0300

    Criação da classe Album.cs.

---
+++ Album.cs
@@ -0,0 +1,9 @@
+﻿class Album
+{
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+    public void AdicionarMusica (Musica musica)
+    {
+
+    }
+}
--- obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1b43104ea8ad2027a3254dcfbda8312053b3d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
